997b43e95a39bf28b55d0aba22f6027cd451715a
sayan7848 <sayan7848@gmail.com>
2018-09-19 16:28:20 +0000

    Xtended bringup
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- xtended_mido.mk
+++ xtended_mido.mk
@@ -21,15 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/aosp/common.mk)
-
 #Boot Animation res
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Inherit some common gzosp stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := aosp_mido
+PRODUCT_NAME := xtended_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
